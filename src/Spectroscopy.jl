72dc80cfe443bdfd9b68982a158bd919434a1ccd
yctai <brianyjtai1994@gmail.com>
2021-11-28 23:30:09 +0800

    Add: type alias

--- src/Spectroscopy.jl
+++ src/Spectroscopy.jl
@@ -1,5 +1,14 @@
 module Spectroscopy
 
+const VecI  = AbstractVector # Input  Vector
+const VecO  = AbstractVector # Output Vector
+const VecB  = AbstractVector # Buffer Vector
+const VecIO = AbstractVector # In/Out Vector
+const MatI  = AbstractMatrix # Input  Matrix
+const MatO  = AbstractMatrix # Output Matrix
+const MatB  = AbstractMatrix # Buffer Matrix
+const MatIO = AbstractMatrix # In/Out Matrix
+
 abstract type TimeAxis end
 
 include("./TimeDelay.jl")
